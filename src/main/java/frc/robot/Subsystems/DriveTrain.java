28988fd67dd4d5e8ae2e4d24ca800003818b0f26
178Laptop <programming@farmingtonrobotics.org>
2020-01-16 16:45:04 -0500

    We added the drive train code.

--- src/main/java/frc/robot/Subsystems/DriveTrain.java
+++ src/main/java/frc/robot/Subsystems/DriveTrain.java
@@ -1,5 +1,47 @@
-package frc.robot.Subsystems;
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
-public class DriveTrain {
+package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+import frc.robot.commands.JoystickDrive;
+
+/**
+ * Add your docs here.
+ */
+public class DriveTrain extends Subsystem {
+  
+  //DM declarations
+  public static VictorSPX left1;
+  public static VictorSPX left2;
+  public static VictorSPX right1;
+  public static VictorSPX right2;
+    
+  public DriveTrain() {
+	  //DM initializations
+	  left1 = new VictorSPX(RobotMap.DMTopLeft);
+	  left2 = new VictorSPX(RobotMap.DMBottomLeft);
+	  right1 = new VictorSPX(RobotMap.DMTopRight);
+	  right2 = new VictorSPX(RobotMap.DMBottomRight);
+  }
+  
+  public void drive(double leftPower, double rightPower) {
+    left1.set(ControlMode.PercentOutput, -leftPower);
+    left2.set(ControlMode.PercentOutput, -leftPower);
+    right1.set(ControlMode.PercentOutput, rightPower);
+    right2.set(ControlMode.PercentOutput, rightPower);
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    setDefaultCommand(new JoystickDrive());
+  }
 }
