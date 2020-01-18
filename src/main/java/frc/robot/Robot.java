dfe3025465efa4be894c480c104d50ab155f4918
178Laptop <programming@farmingtonrobotics.org>
2020-01-18 14:52:48 -0500

    Created a New Gyro Class, fixed some errors

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -23,15 +23,16 @@ public class Robot extends TimedRobot {
 
   public static DriveTrain drivetrain;
   public static OI oi;
-  // private static final String kDefaultAuto = "Default";
-  // private static final String kCustomAuto = "My Auto";
-  // private String m_autoSelected;
-  // private final SendableChooser<String> m_chooser = new SendableChooser<>();
+   private static final String kDefaultAuto = "Default";
+   private static final String kCustomAuto = "My Auto";
+   private String m_autoSelected;
+   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
    */
+  
   @Override
   public void robotInit() 
   {
@@ -67,7 +68,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
+
+  m_autoSelected = m_chooser.getSelected();
     // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
     System.out.println("Auto selected: " + m_autoSelected);
   }
