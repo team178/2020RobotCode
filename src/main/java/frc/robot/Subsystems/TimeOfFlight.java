7ac435b428c98c3b9a71351cf6cbf3b3a4c86e28
Robbie <20fishelro@fpsct.org>
2020-01-25 10:52:54 -0500

    Implemented Time of Flight code from the Sensor Board into this repo

--- src/main/java/frc/robot/Subsystems/TimeOfFlight.java
+++ src/main/java/frc/robot/Subsystems/TimeOfFlight.java
@@ -1,23 +1,38 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.Subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.I2C;
+import libs.ToF.org.letsbuildrockets.libs.TimeOfFlightSensor;
+
+import edu.wpi.first.wpilibj.TimedRobot;
 
 public class TimeOfFlight extends SubsystemBase {
 
-  public TimeOfFlight() {
-
+  private TimeOfFlightSensor tofsensor;
+  /**
+   * Creates a new Timeofflight.
+   */
+  public void robotInit() {
+    tofsensor = new TimeOfFlightSensor(0x621);
+    System.out.println("ToF Sensor at " + String.format("0x%03x", tofsensor.getID()) + "! (with firmware version: " + tofsensor.getFirwareVersion().toString()+")");
   }
 
-  @Override
-  public void periodic() {
+  public double getDistance() {
     // This method will be called once per scheduler run
+    if(tofsensor.inRange()){
+      System.out.println("distance: " + tofsensor.getDistance()+ " " + tofsensor.getError());
+      // distance measured in mm
+      if(tofsensor.getDistance() <= 600){
+        boolean ballHere = true;
+        return 0;
+      }
+      return tofsensor.getDistance();
+    } else {
+      System.out.println("out of range");
+      return -1;
+    }
   }
 }
+
+// websites used for background understandng:
+// https://docs.wpilib.org/en/latest/docs/software/sensors/encoders-software.html
+// https://first.wpi.edu/FRC/roborio/beta/docs/java/edu/wpi/first/wpilibj/Encoder.html
