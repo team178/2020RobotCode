dfc4cbf3d9a429635cad991a5a622b3614d8e9ea
178Laptop <programming@farmingtonrobotics.org>
2020-01-31 16:42:48 -0500

    gyro code updates

--- src/main/java/frc/robot/Subsystems/GyroTest2.java
+++ src/main/java/frc/robot/Subsystems/GyroTest2.java
@@ -4,9 +4,10 @@ import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class GyroTest2 extends SubsystemBase {
-    //All of this code can now be found in the DriveTrain Subsystem
-    //https://frc-pdr.readthedocs.io/en/latest/control/gyro.html
+                                                                                                                                                                                                                                                                                                                                                                     ic class GyroTest2 extends SubsystemBase {
+     
+    //All of this code can now be found in the Drive Train Subsystem
+    //https://frc-pdr.readthedocs.io/en/latest/cont rol/gyro.html
 }
 
 
