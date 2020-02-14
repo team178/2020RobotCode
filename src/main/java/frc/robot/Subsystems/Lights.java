887bcefb0fa26c4a83398301fd88ad70ac9f7386
Seth Frank <21frankse@fpsct.org>
2020-02-14 18:09:09 -0500

    Added lights subsystem

--- src/main/java/frc/robot/Subsystems/Lights.java
+++ src/main/java/frc/robot/Subsystems/Lights.java
@@ -7,6 +7,10 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
@@ -14,9 +18,77 @@ public class Lights extends SubsystemBase {
   /**
    * Creates a new Lights.
    */
-  public Lights() {
+  protected I2C arduino;
+  protected DriverStation ds;
+  protected boolean received;
+  protected boolean sent;
 
+  public Lights(Port port, int address) {
+    arduino = new I2C(port, address);
+    ds = DriverStation.getInstance();
   }
+
+
+  public boolean sendMessage(String pattern) {
+    boolean sent = false;
+    String message = pattern.toLowerCase();
+    
+    //System.out.println("Message Sent: " + message);
+    sent = !arduino.writeBulk(message.getBytes()); //Returns true if aborted, false if completed
+    //System.out.println("Message Sent? " + arduino.addressOnly());
+
+    return sent;
+  }
+
+  public byte[] receiveMessage(int address) //for which i2c address to read from
+  {
+    byte[] dataFromArduino = new byte[2]; //change based on type of data 
+    received = !arduino.read(address, 2, dataFromArduino); //Returns true if aborted, false if completed
+    //System.out.print("Message Received: ");
+    
+    for (byte b : dataFromArduino) { //Receives data in bytes from arduino and converts to binary 
+      String s1 = String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0');
+      //System.out.print(s1 + ", ");
+    }
+    //System.out.println();
+    
+    return dataFromArduino;
+  }
+
+  //Checker methods
+  public boolean checkIfReceived()
+  {
+    return received;
+  }
+
+  public boolean checkIfSent()
+  {
+    return sent;
+  }
+  
+  //Lights methods -- sends characters to arduino to indicate different light colors
+  public boolean setAllianceColor() {
+    if (ds.getAlliance() == Alliance.Blue) {
+      return sendMessage("b");
+    }
+    else {
+      return sendMessage("r");
+    }
+  }
+
+  public boolean lights1() {
+    return sendMessage("h");
+  }
+
+  public boolean lights2() {
+    return sendMessage("c");
+  }
+
+  public boolean lightsOff()
+  {
+    return sendMessage("n");
+  }
+
 
   @Override
   public void periodic() {
